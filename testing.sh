cca873412617dd780acf7df47bf8ec65174374a6
Gabriela Porto <gabriela.bairros.porto96@gmail.com>
2021-05-17 18:47:43 -0300

    Adicionado diff anyway

--- testing.sh
+++ testing.sh
@@ -5,7 +5,8 @@ make
 
 for i in {0..9}
  do
+	echo $(($i))
     echo Caso 0$(($i)) >> results/caso-0$(($i)).out
     ./analiseIntervalar < input/caso-0$(($i)).in > results/caso-0$(($i)).out
-    echo -------------------------------------------------------
+	diff output/caso-0$(($i)).out results/caso-0$(($i)).out > results/diffcaso-0$(($i))
 done
