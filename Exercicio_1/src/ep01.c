e5abc2faa539d59a7a926d483744dfd268169ba1
Gabriela Porto <gabriela.bairros.porto96@gmail.com>
2021-10-03 19:41:01 -0300

    NEWTON FUNCIONAgit add -A git add -A

--- Exercicio_1/src/ep01.c
+++ Exercicio_1/src/ep01.c
@@ -2,14 +2,22 @@
 #include <matheval.h>
 #include <assert.h>
 #include <string.h>
+#include "floatType.h"
+#include <math.h>
 
 #define TAM_BUFFER 100
 char funcao[TAM_BUFFER];
 double x0;
 double epsilon;
 int maxIter;
+double resultado_anterior_newton, resultado_newton, parada_newton, resultado_anterior_secante,
+resultado_secante, parada_secante, erro_absoluto, erro_relativo, ulps;
+int refino_newton = 0, refino_secante = 0;
+
+void *f, *f_prim;
 
 void leituraVariavel(){
+	
 	fgets(funcao, TAM_BUFFER, stdin);
 	int length = strlen (funcao);
 	if (length > 0 && funcao[length - 1] == '\n')
@@ -17,8 +25,21 @@ void leituraVariavel(){
 	scanf("%lg", &x0);
 	scanf("%lg", &epsilon);
 	scanf("%d", &maxIter);
+
 }
 
+int verification_newton(){
+	parada_newton = fabs(resultado_newton - resultado_anterior_newton);
+	return parada_newton < epsilon;
+}
+
+void metodoNewton(){
+	double divisao = (evaluator_evaluate_x(f,resultado_anterior_newton) / evaluator_evaluate_x(f_prim,resultado_anterior_newton));
+	resultado_newton = resultado_anterior_newton - divisao;
+	if(verification_newton()){
+		refino_newton = 1;
+	}
+}
 
 int main(){
 
@@ -28,25 +49,28 @@ int main(){
 	// esta devolvendo alguma coisa que existe
 	/* Create evaluator for function.  */
 
-	void *f = evaluator_create(funcao);
+	f = evaluator_create(funcao);
 	//printf("%s\n",evaluator_get_string(f));
 	assert (f);
 	
 	/* Create evaluator for function derivative and print textual
 		representation of derivative.  */
-	void *f_prim = evaluator_derivative_x (f);
+	f_prim = evaluator_derivative_x (f);
 
+	resultado_anterior_newton = resultado_anterior_secante = x0;
+	for(int i=0; i< maxIter && (!refino_newton); i++){
+		metodoNewton();
+		printf("%d, %1.16e, %1.16e \n", i, resultado_newton, parada_newton);
+		// printf("%d, %1.16e, %1.16e, %1.16e, %1.16e, %1.16e, %1.16e, %1.16e ", i, resultado_newton,
+		// 	parada_newton, resultado_secante, parada_secante, erro_absoluto, erro_relativo, ulps);
+		resultado_anterior_newton = resultado_newton;
+	}
 
-	printf("%s\n",funcao);
-	printf("%lg\n",x0);
-	printf("%lg\n",epsilon);
-	printf("%d\n",maxIter);
+	// printf("----TERMINO INPUT----\n");
 
-	printf("----TERMINO INPUT----\n");
-
-	printf("  f'(x) = %s\n", evaluator_get_string (f_prim));
-	printf ("  f(%g) = %g\n", x0, evaluator_evaluate_x (f, x0));
-	printf ("  f'(%g) = %g\n", x0, evaluator_evaluate_x (f_prim, x0));
+	// printf("  f'(x) = %s\n", evaluator_get_string (f_prim));
+	// printf ("  f(%g) = %g\n", x0, evaluator_evaluate_x (f, x0));
+	// printf ("  f'(%g) = %g\n", x0, evaluator_evaluate_x (f_prim, x0));
 
 	return 0;
 }
