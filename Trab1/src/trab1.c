23e5571212ae9a7e6e8bf302d55c7a6413fb504a
cesarcamillo <cesaraugustoalvescamillo@hotmail.com>
2021-11-13 23:45:35 -0300

    Corrige leitura de input para nao repetir iteracao e criaçao da matriz de derivadas parciais

--- Trab1/src/trab1.c
+++ Trab1/src/trab1.c
@@ -2,22 +2,28 @@
 #include "utils.h"
 #include "newton.h"
 #include <string.h>
+#include <matheval.h>
+#include <assert.h>
 
 int main(){
 
 	double epsilon, *resultados, **jacobiana, helper;
 	int dim, maxIter, length, end;
 	char equacoes[TAM_BUFFER][TAM_BUFFER], temp[TAM_BUFFER];
+	void *eval, **derivadas;
 
 	if (scanf("%d\n", &dim) == EOF)
 	{
-		printf("Arre egua\n");
-		end = 1;
+		printf("Erro: Arquivo vazio.\n");
+		return -1;
 	}
 
 	while (end != 1)
 	{
+		printf("%d\n", dim);
+
 		resultados = calloc(dim, sizeof(double));
+		derivadas =  (void**)malloc(dim * dim * sizeof(void*));
 
 		for (int i = 0; i < dim; i++)
 		{
@@ -36,13 +42,35 @@ int main(){
 
 		scanf("%lf\n", &epsilon);
 		scanf("%d\n", &maxIter);
-		scanf("\n");
+
+		//Criar vetor das derivadas parciais
+		for (int i = 0; i < dim; i++)
+		{
+			eval = evaluator_create(equacoes[i]);
+			assert(eval);
+			printf("%s = 0\n", evaluator_get_string(eval));
+			for (int j = 0; j < dim; j++)
+			{
+				char variavel[5];
+				sprintf(variavel, "x%d", j);
+				derivadas[(i*dim)+j] = evaluator_derivative(eval, variavel);
+				assert(derivadas[(i*dim)+j]);
+			}
+		}
+
+		printf("#\n");
+
+		//Loop epsilon + maxIter
+		//Criar matriz jacobiana para a resposta atual
+
+		//Aplicar Gauss com Pivoteamento Parcial para refinar a resposta
+		//Fim Loop
 
 		if (scanf("%d\n", &dim) == EOF)
 		{
 			end = 1;
 		}
-		printf("Dimensao: %d\n", dim);
+		printf("\n");
 	}
 	
 	
