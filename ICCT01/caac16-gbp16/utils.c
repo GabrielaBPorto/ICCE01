f9875732319fc5af66eaaabdfda3305167c869bb
Gabriela Porto <gabriela.bairros.porto96@gmail.com>
2021-11-14 20:49:58 -0300

    Terminado trabalho 1

---
+++ ICCT01/caac16-gbp16/utils.c
@@ -0,0 +1,19 @@
+#include "utils.h"
+
+/*  Retorna tempo em milisegundos
+
+    Forma de uso:
+ 
+    double tempo;
+    tempo = timestamp();
+    <trecho de programa do qual se deseja medir tempo>
+    tempo = timestamp() - tempo;
+*/
+
+double timestamp(void)
+{
+  struct timeval tp;
+  gettimeofday(&tp, NULL);
+  return((double)(tp.tv_sec*1000.0 + tp.tv_usec/1000.0));
+}
+
