45b32e7c5417e76564c71ed581a49438d198c519
cesarcamillo <cesaraugustoalvescamillo@hotmail.com>
2021-11-14 01:30:47 -0300

    Calculo da jacobiana feito

--- Trab1/src/trab1.c
+++ Trab1/src/trab1.c
@@ -9,7 +9,7 @@ int main(){
 
 	double epsilon, *resultados, *jacobiana, *resultadoJacobiana, helper, maxJacobiana, maxEval;
 	int dim, maxIter, length, end, iter;
-	char equacoes[TAM_BUFFER][TAM_BUFFER], temp[TAM_BUFFER];
+	char equacoes[TAM_BUFFER][TAM_BUFFER], temp[TAM_BUFFER], **variaveis;
 	void *eval, **derivadas;
 
 	if (scanf("%d\n", &dim) == EOF)
@@ -21,12 +21,16 @@ int main(){
 	while (end != 1)
 	{
 		printf("%d\n", dim);
-		iter = 0;
 
 		resultados = calloc(dim, sizeof(double));
 		resultadoJacobiana = calloc(dim, sizeof(double));
 		derivadas = (void**)malloc(dim * dim * sizeof(void*));
 		jacobiana = calloc(dim*dim, sizeof(double));
+		variaveis = calloc(dim, sizeof(char *));
+		for (int i = 0; i < dim; i++)
+		{
+			variaveis[i] = calloc(5, sizeof(char));
+		}
 
 		for (int i = 0; i < dim; i++)
 		{
@@ -56,6 +60,11 @@ int main(){
 			{
 				char variavel[5];
 				sprintf(variavel, "x%d", j+1);
+				if (i == 0)
+				{
+					length = strlen (variavel);
+					memcpy(variaveis[j], variavel, length);
+				}
 				derivadas[(i*dim)+j] = evaluator_derivative(eval, variavel);
 				assert(derivadas[(i*dim)+j]);
 			}
@@ -65,28 +74,49 @@ int main(){
 
 		//Loop epsilon < max resultadoJacobiana, epsilon < max F(x) , iter < maxIter
 		//exemplo: https://moodle.c3sl.ufpr.br/pluginfile.php/140644/mod_resource/content/16/Resolu%C3%A7%C3%A3o%20de%20Sistemas%20N%C3%A3o-Lineares.pdf slide 38
-
+		for (iter = maxIter-1; iter < maxIter; iter++) //Adicionar os epsilons na verificação ali
+		{
+			for (int i = 0; i < dim; i++)
+			{
+				printf("x%d = %.6lf\n", i+1, resultados[i]);
+			}
 			//Criar matriz jacobiana para a resposta atual, necessário computar o tempo
 			//Para criar a matriz jacobiana, só aplicar jacobiana[(i*dim)+j] = evaluator_evaluate(derivadas[(i*dim)+j], resultados[j])
-			
+			for (int i = 0; i < dim; i++)
+			{	
+				for (int j = 0; j < dim; j++)
+				{
+					jacobiana[(i*dim)+j] = evaluator_evaluate(derivadas[(i*dim)+j], dim, (char **)variaveis, resultados);
+				}
+
+			}		
 
 			//Aplicar Gauss com Pivoteamento Parcial para refinar a resposta, necessário computar o tempo
 			//Exemplo: https://www.bragitoff.com/2018/02/gauss-elimination-c-program/
 
 			//Calcular novo resultado (resultados[i] += resultadoJacobiana[i])
 			//Encontrar maior F(X) e maior resultadoJacobiana
+		}
 		//Fim Loop
+
+		//Fazer free de tudo que precisa aqui
+		free(derivadas);
+		free(jacobiana);
+		free(resultados);
+		free(resultadoJacobiana);
+		for (int i = 0; i < dim; i++)
+		{
+			free(variaveis[i]);
+		}
+		free(variaveis);
+
+		//Printar os tempos
 
 		if (scanf("%d\n", &dim) == EOF)
 		{
 			end = 1;
 		}
 		printf("\n");
-		//Fazer free de tudo que precisa aqui
-		free(derivadas);
-		free(jacobiana);
-		free(resultados);
-		free(resultadoJacobiana);
 	}
 	
 	
