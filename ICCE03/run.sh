c00e40fce08b2d8540be345f46379db9fff01c34
Gabriela Porto <gabriela.bairros.porto96@gmail.com>
2021-12-05 15:42:49 -0300

    Mudando python

--- ICCE03/run.sh
+++ ICCE03/run.sh
@@ -48,37 +48,37 @@ plot() {
   done
 }
 
-plot_time() {
-  for nBand in ${nBands[@]}; do
-    group=$1
-    echo "set logscale x 2" >> $group.data
-    echo "set logscale y" >> $group.data
-    echo "set format x '2^{%L}'" >> $group.data
-    echo "set key left" >> $group.data
-    echo "set grid" >> $group.data
-    echo "set xlabel \"n\"" >> $group.data
-    echo "set ylabel \"$3\"" >> $group.data
-    echo "set title \"$4\"" >> $group.data
-    echo "set terminal png" >> $group.data
-    echo "set output \"$group.png\"" >> $group.data
-    echo -n "plot 'tmp' u 1:2 w l lt rgb 'red'   lw 2 title 'v0-mvv', 'tmp' u 1:3 w l lt rgb 'gold'   lw 2  title 'v0-mmv' ,\
-                  'tmp' u 1:4 w l lt rgb 'green'  lw 2  title 'v4-mvv', 'tmp' u 1:5 w l lt rgb 'purple' lw 2 title 'v4-mmv' " >> $group.data
-    for n in ${ns[@]}; do
-      echo -n "$n " >> tmp
-      loop=$(cat $n.res | grep "Tempo método CG" | cut -d":" -f2 | cut -d"<" -f2 | cut -d">" -f1 | sed -e 's/ //g')
-      mvv=$(cat $n.FLOPS_AVX | grep "$2" | cut -d\| -f 3 | tr '\n' '\t' | cut -f 1 | sed -e 's/ //g')
-      mmv=$(cat $n.FLOPS_AVX | grep "$2" | cut -d\| -f 3 | tr '\n' '\t' | cut -f 2 | sed -e 's/ //g')
-      x=$(echo "$mvv * 100" | bc -l | awk '{printf "%f", $0}')
-      y=$(echo "$mmv * 100" | bc -l | awk '{printf "%f", $0}')
-      echo -n "$x $y " >> tmp
-      rm sistema_$n.res
-      echo "" >> tmp
-    done
-    gnuplot $group.data
-    rm tmp
+# plot_time() {
+#   for nBand in ${nBands[@]}; do
+#     group=$1
+#     echo "set logscale x 2" >> $group.data
+#     echo "set logscale y" >> $group.data
+#     echo "set format x '2^{%L}'" >> $group.data
+#     echo "set key left" >> $group.data
+#     echo "set grid" >> $group.data
+#     echo "set xlabel \"n\"" >> $group.data
+#     echo "set ylabel \"$3\"" >> $group.data
+#     echo "set title \"$4\"" >> $group.data
+#     echo "set terminal png" >> $group.data
+#     echo "set output \"$group.png\"" >> $group.data
+#     echo -n "plot 'tmp' w l lt rgb 'red'   lw 2 title 'v0-mvv', 'tmp' u 1:3 w l lt rgb 'gold'   lw 2  title 'v0-mmv' ,\
+#                   'tmp' w l lt rgb 'green'  lw 2  title 'v4-mvv', 'tmp' u 1:5 w l lt rgb 'purple' lw 2 title 'v4-mmv' \" >> $group.data
+#     for n in ${ns[@]}; do
+#       echo -n "$n " >> tmp
+#       loop=$(cat $n.res | grep "Tempo método CG" | cut -d":" -f2 | cut -d"<" -f2 | cut -d">" -f1 | sed -e 's/ //g')
+#       mvv=$(cat $n.FLOPS_AVX | grep "$2" | cut -d\| -f 3 | tr '\n' '\t' | cut -f 1 | sed -e 's/ //g')
+#       mmv=$(cat $n.FLOPS_AVX | grep "$2" | cut -d\| -f 3 | tr '\n' '\t' | cut -f 2 | sed -e 's/ //g')
+#       x=$(echo "$mvv * 100" | bc -l | awk '{printf "%f", $0}')
+#       y=$(echo "$mmv * 100" | bc -l | awk '{printf "%f", $0}')
+#       echo -n "$x $y " >> tmp
+#       rm sistema_$n.res
+#       echo "" >> tmp
+#     done
+#     gnuplot $group.data
+#     rm tmp
 
-  done
-}
+#   done
+# }
 
 for n in ${ns[@]}; do
   ./gerarSL $n >>  $diretorio/sistema_$n.res
@@ -96,5 +96,6 @@ plot_time "TIME" "RDTSC Runtime" "ms" "Runtime"
 
 
 echo "powersave" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor 
+cd /home/gabi/Faculdade/ICC/ICCE01/ICCE03
 make clean;
 make purge faxina;
--- ICCE03/script.py
+++ ICCE03/script.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 import seaborn as sns
-expected_sizes = [64, 100, 128, 500, 516,1000, 1024, 2000, 2048, 3000, 4096, 5000]
+expected_sizes = [10, 32]
 ## expected_sizes = [64, 100, 128, 500]
 #expected_sizes = [5,10]
 
@@ -12,13 +12,13 @@ diretorio='./results'
 # %%
 def getGroupTable(group: str, size: int, columns: list = None) -> pd.DataFrame:
     # Executa likwid no terminal
-	./gerarSL $n >>  $diretorio/sistema_$n.in
-    cmd = likwid-perfctr -C 3 -g $group -f -m ./gaussJacobi-likwid $diretorio/sistema_$n.in > $diretorio/group.csv
+
+    cmd = f"likwid-perfctr -C 3 -g { group} -o { diretorio}/{ size }.{ group}.csv -f -m ./gaussJacobi-likwid { diretorio}/sistema_{ size }.res"
     print (cmd)
     os.system(cmd)
 
     # Carrega arquivo de resultado
-    file_name = f'./Resultados/result{group}_{size}.csv'
+    file_name = f"{diretorio}/{ size }.{ group}.csv"
     result = open(file_name, 'r')
 
     # Filtra arquivo de resultado
@@ -34,11 +34,6 @@ def getGroupTable(group: str, size: int, columns: list = None) -> pd.DataFrame:
                 tables[currentTable] = {}
         elif lineSplit[0] == "STRUCT":
             currentTable = 'trash'
-        else:
-            try:
-                tables[currentTable][lineSplit[0]] = float(lineSplit[1])
-            except:
-                tables[currentTable][lineSplit[0]] = lineSplit[1]
     # Remove lixo do lido
     tables.pop('trash')
 
@@ -53,20 +48,12 @@ def getGroupTable(group: str, size: int, columns: list = None) -> pd.DataFrame:
 
 
 # %%
-tabelados = {}
-resultados = {}
-for size in expected_sizes:
-    cmdCurva = f"./gera_entrada {size} > ./Resultados/gerado{size}.in; ./curvas <./Resultados/gerado{size}.in > ./Resultados/gerado{size}.out"
-    os.system(cmdCurva)
-    tabelados[size]=pd.read_csv(f'./Resultados/gerado{size}.in',skiprows=1, sep=' ', header = None).iloc[:,:-1]
-    resultados[size]= pd.read_csv(f'./Resultados/gerado{size}.out', sep=' ', header = None).iloc[:,:-1]
-    cmdLikwid= f"./gera_entrada {size} > ./Resultados/gerado{size}.in; ./curvas <./Resultados/gerado{size}.in > ./Resultados/gerado{size}.out"
-
-
-# %%
 testColumns = ['L2 miss ratio', 'Runtime (RDTSC) [s]','call count']+['L3 bandwidth [MBytes/s]']+['DP MFLOP/s', 'AVX DP MFLOP/s']
 dfFinal = pd.DataFrame(columns = testColumns,dtype=np.float64)
 for size in expected_sizes:
+    cmdEntrada = f"./geraSL {size} > results/sistema_{size}.res"
+    print (cmdEntrada)
+    os.system(cmdEntrada)
     L2CACHE_table = getGroupTable('L2CACHE', size, ['L2 miss ratio', 'Runtime (RDTSC) [s]','call count'])
     L3_table = getGroupTable('L3', size, ['L3 bandwidth [MBytes/s]'])
     FLOPS_DP_table = getGroupTable('FLOPS_DP', size, ['DP MFLOP/s', 'AVX DP MFLOP/s'])
@@ -76,40 +63,12 @@ for size in expected_sizes:
     dfFinal=dfFinal.append(dfr)
 
 dfFinal[['region', 'n_size']]= dfFinal.index.tolist()
-dfFinal.to_csv("./Resultados/tabelaFinal.csv")
+dfFinal.to_csv("./results/tabelaFinal.csv")
 for col in testColumns:
     dfFinal.loc[:,col]=dfFinal.loc[:,col].apply(lambda x :float(x))
     try:
-        path = os.path.join('./Resultados/',col.replace('/','p').replace(' ','_')+'.png')
+        path = os.path.join('./results/',col.replace('/','p').replace(' ','_')+'.png')
         sns.lineplot(data =dfFinal, x='n_size', y =col, hue='region').figure.savefig(path)
         plt.close()
     except:
         pass
-
-
-# Para cada n esperado
-for item in expected_sizes:
-    m = len(tabelados[item])-1
-    xTab = tabelados[item].loc[0]
-    # Para cada função com valor tabelado
-    for iM in range(m):
-        yTab = tabelados[item].loc[iM+1]
-        intervalX=max(xTab)- min(xTab)
-        # Margem de 5% no valor do plot do eixo x
-        padding=intervalX*0.05
-        xs = np.linspace(min(xTab), max(xTab)+padding, 1000)
-        df = pd.DataFrame(columns=["interpolador", "ajuste"], index=xs)
-        interpol = resultados[item].loc[iM*2]
-        ajuste = resultados[item].loc[iM*2+1]
-        df['interpolador'] = polynomial_coefficients(xs, interpol)
-        df['ajuste'] = polynomial_coefficients(xs, ajuste)
-        xsG = np.linspace(-5, 5, 10)
-        df.plot(style= ['-','-.'])
-	        
-        plt.scatter(xTab,yTab, color = 'black',  label='Pontos Tabelados',marker = 'x' )
-        plt.legend(loc='best')
-        plt.xscale("log")
-        plt.title(f"Valores tabelados = {item}, Funçao Tabelada {iM}")
-        path = os.path.join('./Resultados/',f'n{item}f[{iM}].png')
-        plt.savefig(path)
-        plt.close()
