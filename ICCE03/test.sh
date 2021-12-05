2e6d55b67cdcca616de445aeb59409e61fcc2b97
cesarcamillo <cesaraugustoalvescamillo@hotmail.com>
2021-12-05 00:43:45 -0300

    Pau no cu do makefile

--- ICCE03/test.sh
+++ ICCE03/test.sh
@@ -81,7 +81,7 @@ plot_time() {
 }
 
 for n in ${ns[@]}; do
-  #./gerarSL $n >>  $diretorio/sistema_$n.res
+  ./gerarSL $n >>  $diretorio/sistema_$n.res
   for group in ${groups[@]}; do
     likwid-perfctr -C 3 -g $group -f -m ./gaussJacobi-likwid $diretorio/sistema_$n.res > $diretorio/$n.$group
   done
